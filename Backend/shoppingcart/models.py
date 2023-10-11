8b9a78ce7240d2d588c9a9c7d0bc97d260d5a76b
Camilo Montaño <cristian.camilo.montano@correounivalle.edu.co>
2023-10-11 12:18:10 -0500

    first commit

---
+++ Backend/shoppingcart/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from utils import db
+
+# Create your models here.
+
+person_collection = db['Person']
---
+++ Backend/utils.py
@@ -0,0 +1,17 @@
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://shoppingcart:kPbvbpAXcFMsezkE@shoppingcart.jd7punl.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+db = client['test_mongo']
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
