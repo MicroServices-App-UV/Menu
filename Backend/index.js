09bac38b52cd51b5cf06b6f84f7629b7c5b91c9b
Santiagospitia <ospitia2002@gmail.com>
2023-12-18 15:17:46 -0500

    docker compose

--- Backend/index.js
+++ Backend/index.js
@@ -1,28 +1,50 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
-const rutas = require('./routes/routes.js')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const rutas = require("./routes/routes.js");
+const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://shoppingcart:kPbvbpAXcFMsezkE@shoppingcart.jd7punl.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://shoppingcart:kPbvbpAXcFMsezkE@shoppingcart.jd7punl.mongodb.net/?retryWrites=true&w=majority";
 const app = express();
-const port = 3333
-app.use(express.json());
+const port = 3333;
 
-
-app.use('/api', rutas)
-
-app.get('/', (req,res) => {
-    res.send('API Menu Working ');
+// Configuración de Winston
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.Http({
+      host: "logstash",
+      port: 9600, // Puerto de Logstash
+      path: "/", // Ruta de entrada de Logstash
+      ssl: false,
+    }),
+  ],
 });
 
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use(express.json());
+
+app.use("/api", rutas);
+
+app.get("/", (req, res) => {
+  res.send("API Menu Working ");
+});
 
 mongoose.set("strictQuery", false);
 
 mongoose
-    .connect(uri)
-    
-    .then(() => { console.log('connected to MongoDB atlas')});
-    //.catch((error) => console.error(error));
+  .connect(uri)
 
-app.listen(port, () => {console.log('server listing on port', port); }) 
+  .then(() => {
+    console.log("connected to MongoDB atlas");
+  });
+//.catch((error) => console.error(error));
 
+app.listen(port, () => {
+  console.log("server listing on port", port);
+});
